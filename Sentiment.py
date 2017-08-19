b7b88b1f19ffb22d91ec9b22f2e7a90215ce96b0
Sohel Dhengre <dhengre_sohel.ghrceit@raisoni.net>
2017-08-19 14:00:29 +0530

    Update Sentiment.py

--- Sentiment.py
+++ Sentiment.py
@@ -1,11 +1,11 @@
 from textblob import TextBlob
 import tweepy
 
-consumer_key = '8fyYqm49CTRR5H0JRkhxH0f2s'
-consumer_secret = 'eM5L0a0WYzZtT8r3quBiE4SVOz1IjZYHcST1gyMITcY46DHLeM'
+consumer_key = '8fyYqm49CTRR5H0JRkhx####'
+consumer_secret = 'eM5L0a0WYzZtT8r3quBiE4SVOz1IjZYHcST1gy####'
 
-access_token = '4015739903-dq9HrBPcWmS4Lfulwcx9gBqrAG73M3neZw5BnTY'
-access_token_secret = 'todlONeA68QUgmPYRuKMz8EhyYEdFBlE7Ys84pRh3yxoX'
+access_token = '4015739903-dq9HrBPcWmS4Lfulwcx9gBqrAG73######'
+access_token_secret = 'todlONeA68QUgmPYRuKMz8EhyYEdFBlE#######'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
